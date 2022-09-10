d147ae86084205183eefdb9ffbc486f6c13358a8
vincent okoye <vincentokoye4444@gmail.com>
2022-09-10 13:01:16 -0700

    my c commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
-
-
 /**
  * main - Entry point
- * - Dora korpar, 2015-10-19
- * Description: display output without using printf name, puts name.
- * then a new line to thestandard error
- * Return: Always 0
- */
-int main(void)
-{
-	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
-	return (1);
-}
+ *
+ * Description outputs the standard error without the use of printf
+ *
+ * Return: 1
+ * /
+  int main(void)
+ {
+	 write(2, "and that piece of art is useful\" - Dora Korpa, 2015-10-19"' 59);
+	 return (1);
+ }
