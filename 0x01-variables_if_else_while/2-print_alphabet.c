dd581d68eee0f98736158cdcf60dbb41a7d7ed58
vincent okoye <vincentokoye4444@gmail.com>
2022-09-11 14:46:44 -0700

    alphabet commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: print out alphabets
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; a++)
+	{
+		putchar(x);
+	}
+	return (0);
+}
