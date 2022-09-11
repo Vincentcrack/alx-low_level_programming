23fa049340020312aeeb91a2355d26852319fc34
vincent okoye <vincentokoye4444@gmail.com>
2022-09-11 10:22:16 -0700

    my var commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: creating a variable
+ *
+ * Return: 0
+ */
+int main(void)
+{
+		int n;
+
+		srand(time(0));
+		n = rand() - RAND_MAX / 2;
+		if (n > 0)
+		{
+			printf("is positive\n");
+		}
+		else if (n == 0)
+		{
+			printf("is zero\n");
+		}
+		else if (n < 0)
+		{
+			printf("is negative\n");
+		}
+		return (0);
+}
