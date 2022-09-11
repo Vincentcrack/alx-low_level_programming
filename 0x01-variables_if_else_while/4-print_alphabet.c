621b1467410b7eca5fc0ae6ff29046a6db5897b1
vincent okoye <vincentokoye4444@gmail.com>
2022-09-11 16:48:26 -0700

    send

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -Entry point
+ *
+ * Description: selecting chars
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x != 'e' && x != 'z')
+		{
+			putchar(x);
+		}
+		else
+		{
+
+		}
+	}
+}
