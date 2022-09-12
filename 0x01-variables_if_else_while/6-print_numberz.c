be6c4f01a064c31d4ccc7aa478d68ec2965c4131
vincent okoye <vincentokoye4444@gmail.com>
2022-09-12 03:02:23 -0700

    printing int using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: print out numbers from base 10 using only putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar((x % 10) + '0');
+
+	}
+	putchar("\n");
+	return (0);
+}
