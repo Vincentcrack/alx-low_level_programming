c079e5a402c824a47717fe555151e0c139895a32
vincent okoye <vincentokoye4444@gmail.com>
2022-09-12 00:49:03 -0700

    print out numbers from base 10 starting with 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: print out all numbers from base 10
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d", x);
+	}
+	printf('\n');
+	return (0);
+}
