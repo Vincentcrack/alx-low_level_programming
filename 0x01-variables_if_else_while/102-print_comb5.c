5a9b465c62db24ae9d2e3779aea3271db145dfc6
vincent okoye <vincentokoye4444@gmail.com>
2022-09-12 17:56:42 -0700

    my commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,8 +18,8 @@ int main(void)
 			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
 			putchar(' ');
-			putchar((num 2 / 10) + '0');
-			putchar((num 2 % 10) + '0');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
 			if (num1 == 98 && num2 == 99)
 				continue;
