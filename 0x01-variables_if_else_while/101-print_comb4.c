0b7d740847e9ac12f7d88844c107669baa781061
vincent okoye <vincentokoye4444@gmail.com>
2022-09-12 17:40:23 -0700

    my commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entrt point
+ *
+ * Description: print combination of 3 numbera
+ *
+ * Return: 0
+ */
+int mai (void)
+{
+	int num1, num2, num3;
+
+	for (num1 = 0; num1 <= 7; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 8; num2++)
+		{
+			for (num3 = num2 + 1; num3 <= 9; num3++)
+			{
+				putchar((num1 % 10) + '0');
+				putchar((num2 % 10) + '0');
+				putchar((num3 % 10) + '0');
+
+				if (num1 == 7 && num2 == 8 && num3 == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	returm (0);
+}
