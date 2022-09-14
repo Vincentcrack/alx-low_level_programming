d65e21382ca814e34004e047183d298b25bec900
vincent okoye <vincentokoye4444@gmail.com>
2022-09-14 08:22:29 -0700

    my commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit - is the function
+ * @n: is the int to check
+ * Description: return the last int
+ * Return: 0 or 1
+ */
+int print_last_digit(int n)
+{
+	int x;
+
+	if (n < 0)
+		x = -1 * (x % 10);
+
+	else
+		x = n % 10;
+
+	_putchar(x + '0');
+	return (x);
+}
