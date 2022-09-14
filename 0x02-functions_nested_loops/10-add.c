fe1cf8f685a56594d95ad29b61a1a97d1015d654
vincent okoye <vincentokoye4444@gmail.com>
2022-09-14 11:08:38 -0700

    my commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - function
+ * Description: returns the sum of two int
+ * @x: int num
+ * @c: int num
+ * Return: 0
+ */
+int add(int x, int c)
+{
+	return (x + c);
+}
