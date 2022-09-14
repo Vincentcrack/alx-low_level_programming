c3a051025b35ed0b82cf4f2cbe5a72bbeb88730a
vincent okoye <vincentokoye4444@gmail.com>
2022-09-13 20:38:01 -0700

    mycommit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main"
+/**
+ * _abs - the fuction
+ * @n: the int to check
+ * Description: return the main value
+ * Return: 0 or 1
+ */
+int _abs(int n)
+{
+	return (n >= ? n : n * -1);
+}
