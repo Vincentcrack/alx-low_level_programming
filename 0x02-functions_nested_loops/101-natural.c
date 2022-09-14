66579f3d630596f0939e9a53664b7d48ec6d78fe
vincent okoye <vincentokoye4444@gmail.com>
2022-09-14 12:37:50 -0700

    mycommit

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - the main block
+ * Description: computes and prints the sum
+ * 3 and 5 below 1024
+ * Return: 0
+ */
+int void main(void)
+{
+	int c = 0;
+	int sum = 0;
+
+	while (c < 1024)
+	{
+		if (c % 3 == 0 || c % 5 == 0)
+		{
+			sum += c;
+		}
+		c++;
+	}
+	printf("%i\n", sum);
+	return (0);
+}
