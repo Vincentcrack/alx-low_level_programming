3de7ab7c1448cac22a00b2bb6ab6646d00e1d44f
vincent okoye <vincentokoye4444@gmail.com>
2022-09-14 13:08:37 -0700

    mycommit

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Description: print first 100 fibonacci numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	unsigned long int a = 0, b = 1, x = 0;
+
+	while (i < 98)
+	{
+		x = a = b;
+		a = b;
+		b = x;
+		printf("%lu", x);
+
+		if (i < 97)
+			printf(", ");
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
