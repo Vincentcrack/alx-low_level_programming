b9c61f79402d5c119ac11924c56f57e789a41ddd
vincent okoye <vincentokoye4444@gmail.com>
2022-09-13 18:55:42 -0700

    my commit message

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - void function
+ *
+ * Description: print alphabets in lowwer case
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 'a'; x < 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar(\'n');
+}
