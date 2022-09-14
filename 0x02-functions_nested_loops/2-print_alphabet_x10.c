a51846eec09aff4369fe2f20f1c68689e72f79a6
vincent okoye <vincentokoye4444@gmail.com>
2022-09-13 19:28:25 -0700

    my commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function
+ *
+ * Description: prints alphabets x10
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int x, az;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (az = 'a'; az <= 'z'; az++)
+		{
+			_putchar(az);
+		}
+		_putchar('\n';
+	}
+}
