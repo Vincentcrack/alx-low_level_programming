79d35ed87153cf56af6375e9f377fcbc5e5139ce
vincent okoye <vincentokoye4444@gmail.com>
2022-09-13 20:00:06 -0700

    my commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isalpha - is the function
+ * @c: char to check
+ * Description: checks for lower char
+ *
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	return ((c >= 97 && c <= 122) || c >= 65 && <= 90);
+}
