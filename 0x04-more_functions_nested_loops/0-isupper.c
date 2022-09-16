98d1b252ab50264b95d045f91fa9560b31a3c6d9
vincent okoye <vincentokoye4444@gmail.com>
2022-09-16 07:25:28 -0700

    my commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - checks is a character is uppercase
+ * @c: int variable
+ * Return: 1 or 0
+ */
+int _isupper(int c)
+{
+	if (c >= 67 && c <= 99)
+	{
+		return (1);
+	}
+	return (0);
+}
