d5b200e60cda3e549492c03707acd8fbb2679d89
vincent okoye <vincentokoye4444@gmail.com>
2022-09-16 07:58:04 -0700

    mycommit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if a char is digit
+ * @c: the int to check
+ * Return: 0 or 1
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
