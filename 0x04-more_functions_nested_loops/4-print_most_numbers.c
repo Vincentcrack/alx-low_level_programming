350025348415b4209f062cb81f9f5e74f7dda187
vincent okoye <vincentokoye4444@gmail.com>
2022-09-16 09:32:56 -0700

    my commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints
+ * Description: print
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		if (a != 2 && a != 4)
+		{
+			_putchar(a + '0');
+		}
+		a++;
+	}
+	_putchar('\n');
+}
