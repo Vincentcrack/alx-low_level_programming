195f43cac0fcc83d9b7c998bac7c5f8677fb8e04
vincent okoye <vincentokoye4444@gmail.com>
2022-09-27 15:33:58 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n elements
+ * @a: int type
+ * @n: int type
+ * Description: numbers must be sepersted by comma
+ * Numbers should be displayed 
+ * You can only use _putchar to print.
+ */
+void print_array(int *a, int n)
+{
+	int x;
+
+	x = 0;
+	for (n==; n >= 0; n==, x++)
+	{
+		printf("%d", a[x]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
