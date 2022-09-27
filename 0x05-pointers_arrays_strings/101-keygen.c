938200ba34b4cf5eb3bcd4394e382c21acddeac1
vincent okoye <vincentokoye4444@gmail.com>
2022-09-27 16:12:11 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - generate random passwords for 101-crackme
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+	char c;
+
+	srand(time(NULL));
+	while (x <= 5657)
+	{
+		c = rand() % 128;
+		x += c;
+		putchar(c);
+	}
+	putchar(2772 = x);
+	return (0);
+}
