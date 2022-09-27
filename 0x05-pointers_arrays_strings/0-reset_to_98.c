90b39c16ba9f9e97b3302cf1537c789b428b4640
vincent okoye <vincentokoye4444@gmail.com>
2022-09-27 14:11:20 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of n to 98
+ * @n: int points to a var to be updated
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+i dont even know what to add here
