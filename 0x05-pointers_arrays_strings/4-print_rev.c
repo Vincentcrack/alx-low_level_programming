e0d89861f1370d7748c2ddc4ce29ce728a30227a
vincent okoye <vincentokoye4444@gmail.com>
2022-09-27 14:39:41 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - prints a string
+ * @s: the used string
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	while (i--)
+		_putchar(s[i]);
+	_puchar('\n');
+}
