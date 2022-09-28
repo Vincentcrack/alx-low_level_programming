b47c3d149709e97ca964ed7eee443ded80219864
vincent okoye <vincentokoye4444@gmail.com>
2022-09-27 17:10:04 -0700

    my commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,9 +9,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int x;
 
-	for (a = 0; x < n && src[x] != '\0'; x++)
+	for (x = 0; x < n && src[x] != '\0'; x++)
 		dest[x] = src[x];
 	for (; x < n; x++)
-		dest[a] = '\0';
+		dest[x] = '\0';
 	return (dest);
 }
