b9e6de7ca41ee56236057f4be4852e57763e08c4
vincent okoye <vincentokoye4444@gmail.com>
2022-10-11 16:47:00 -0700

    Not funny

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dog - basic structure
  * @name: name of the dog
@@ -14,6 +17,11 @@ struct dog
 	float *age;
 	char *owner;
 };
+
+/**
+ * dog_t - typedef for dog struct
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
