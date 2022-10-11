a1ea9cedf144ec0b0d7223b0a0ce9430d1db4924
vincent okoye <vincentokoye4444@gmail.com>
2022-10-11 16:39:55 -0700

    Not funny

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - basic structure
+ * @name: name of the dog
+ * @age: the dog age
+ * @owner: the dog owner
+ * Description: user defined data type for a dog
+ */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
