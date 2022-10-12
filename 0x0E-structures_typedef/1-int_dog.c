9e1698f974c5955d8fee774f6b7b05ebbf95d26f
vincent okoye <vincentokoye4444@gmail.com>
2022-10-11 17:02:13 -0700

    Very hard

--- 0x0E-structures_typedef/1-int_dog.c
+++ 0x0E-structures_typedef/1-int_dog.c
@@ -1,7 +1,5 @@
 #include "dog.h"
 
-#define NULL (void *)0
-
 /**
  * int_dog - initialize a variable of type struct
  * @d: a pointer to struct dog
@@ -14,8 +12,9 @@
 void int_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		return;
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
 }
