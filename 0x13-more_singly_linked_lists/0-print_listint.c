36bef4b892dd8fd7d8c753f7b7e6409603f694cf
vincent okoye <vincentokoye4444@gmail.com>
2022-10-25 11:20:53 -0700

    New Commit.

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_listint - print elements of a singly liked list.
+ * @h: Pointer to a list.
+ * Return: int.
+ */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t **tp;
+	unsigned int cntr = 0;
+
+	tp = h;
+	while (tp)
+	{
+		printf("%d\n", tp->n);
+		cntr++;
+		tp = tp->next;
+	}
+	return (cntr);
+}
