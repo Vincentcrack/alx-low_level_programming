cbd4f355a874b5e7d683b8d1ce6a69d63ec41dec
vincent okoye <vincentokoye4444@gmail.com>
2022-10-25 13:04:14 -0700

    correction

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.c"
+
+/**
+ * listint_len - calculate the number of elements
+ * @h: pointer to list
+ * Return: int
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
